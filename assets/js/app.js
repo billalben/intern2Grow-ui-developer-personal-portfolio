5d700e4571bb90f3254f1c93fba5545d5a9c59d5
Benzazoua <billalben252@gmail.com>
2024-06-02 15:40:58 +0100

    Media Query & menu bar functionality

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,26 @@
 "use strict";
+
+const addEventOnElements = (elements, eventType, callback) => {
+  for (const element of elements) {
+    element.addEventListener(eventType, callback);
+  }
+};
+
+// Nav bar toggle
+const navbar = document.querySelector("[data-navbar]");
+const navTogglers = document.querySelectorAll("[data-nav-toggler]");
+const overlay = document.querySelector("[data-overlay]");
+
+const toggleNavbar = () => {
+  if (window.innerWidth >= 992) return;
+
+  navbar.classList.toggle("active");
+  overlay.classList.toggle("active");
+  document.body.classList.toggle("active");
+};
+
+addEventOnElements(navTogglers, "click", toggleNavbar);
+
+// Close navbar when clicking on link
+const navLinks = document.querySelectorAll("[data-nav-link]");
+addEventOnElements(navLinks, "click", toggleNavbar);
